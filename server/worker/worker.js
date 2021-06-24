19b30ee5693ec7aa6e4f826c5365381be8eacc43
HenB13 <henrikbreines@gmail.com>
2021-06-25 01:58:31 +0200

    comments

--- server/worker/worker.js
+++ server/worker/worker.js
@@ -1,10 +1,5 @@
 const schedule = require("node-schedule");
-// const pg = require("pg");
-// const pool = new pg.Pool();
 const refeshDb = require("./tasks/refresh-db");
-// const DB = require("../db/db");
-// const getEpisodeNumber = require("../lib/getEpisodeNumber");
-// const getSpotifyEpisodes = require("../lib/getSpotifyEpisodes");
 require("dotenv").config();
 // eslint-disable-next-line no-undef
 const { CRON_INTERVAL } = process.env;
