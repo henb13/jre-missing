70f76c353603441e0398ea087bba354ca42569fa
HenB13 <henrikbreines@gmail.com>
2022-02-04 14:51:52 +0100

    bump definition of newly removed to within 14 days

--- client/src/components/EpisodeDetails.js
+++ client/src/components/EpisodeDetails.js
@@ -3,19 +3,18 @@ import { differenceInDays, parseISO } from "date-fns";
 import styles from "./EpisodeDetails.module.css";
 
 const EpisodeDetails = ({ removedDate }) => {
-  const isNewlyRemoved =
-    differenceInDays(new Date(), parseISO(removedDate)) < 7;
-  const dateTimeValue = getClientLocalTime(removedDate, "yyyy-MM-dd");
-  const removedDateString = getClientLocalTime(removedDate, "PPP");
+    const isNewlyRemoved = differenceInDays(new Date(), parseISO(removedDate)) < 14;
+    const dateTimeValue = getClientLocalTime(removedDate, "yyyy-MM-dd");
+    const removedDateString = getClientLocalTime(removedDate, "PPP");
 
-  return (
-    <>
-      {isNewlyRemoved && <span className={styles.new}>new</span>}
-      <span className={styles.removed}>
-        date removed: <time dateTime={dateTimeValue}>{removedDateString}</time>
-      </span>
-    </>
-  );
+    return (
+        <>
+            {isNewlyRemoved && <span className={styles.new}>new</span>}
+            <span className={styles.removed}>
+                date removed: <time dateTime={dateTimeValue}>{removedDateString}</time>
+            </span>
+        </>
+    );
 };
 
 export default EpisodeDetails;
