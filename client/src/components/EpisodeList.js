ac2b9539fe9e213a5bad4abbd5a6fa5f3d0a22ee
HenB13 <henrikbreines@gmail.com>
2022-02-09 20:09:42 +0100

    add separator between episodes on small screens

--- client/src/components/EpisodeList.js
+++ client/src/components/EpisodeList.js
@@ -9,16 +9,23 @@ const EpisodeList = ({ episodesShown, shouldShake }) => {
 
     return (
         <ul className={classesEpList}>
-            {episodesShown.map(({ episode_number, full_name, date_removed }) => (
-                <Episode
-                    number={episode_number}
-                    name={full_name}
-                    removedDate={date_removed}
-                    key={full_name + episode_number}
-                />
+            {episodesShown.map(({ episode_number, full_name, date_removed }, i) => (
+                <>
+                    <Episode
+                        number={episode_number}
+                        name={full_name}
+                        removedDate={date_removed}
+                        key={full_name + episode_number}
+                    />
+                    {i !== episodesShown.length - 1 && <Border />}
+                </>
             ))}
         </ul>
     );
+};
+
+const Border = () => {
+    return <span className={styles.Border}></span>;
 };
 
 export default EpisodeList;
