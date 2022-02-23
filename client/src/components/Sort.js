f263ed0539c58b6754384e5540bf33a3dd0a93e0
HenB13 <henrikbreines@gmail.com>
2022-02-23 16:23:33 +0100

    revert #4455963 for Sort.js
    So the order (reverse) is kept when switching between sort options

--- client/src/components/Sort.js
+++ client/src/components/Sort.js
@@ -1,4 +1,4 @@
-import { ReactComponent as ArrowIcon } from "../icons/arrow.svg";
+import { ReactComponent as Arrow } from "../icons/arrow.svg";
 import { useEffect, useState } from "react";
 import classnames from "classnames";
 import styles from "./Sort.module.css";
@@ -6,10 +6,12 @@ import styles from "./Sort.module.css";
 const Sort = ({ setEpisodesShown }) => {
     const options = ["episode number", "date removed"];
     const [sortOption, setSortOption] = useState(options[0]);
+    const [reverse, setReverse] = useState(false);
 
     useEffect(() => {
         setEpisodesShown((episodesShown) => {
             return [...episodesShown].sort((a, b) => {
+                [a, b] = reverse ? [a, b] : [b, a];
                 switch (sortOption) {
                     case "episode number":
                         return a.episode_number - b.episode_number;
@@ -20,70 +22,51 @@ const Sort = ({ setEpisodesShown }) => {
                 }
             });
         });
-    }, [sortOption, setEpisodesShown]);
+    }, [sortOption, setEpisodesShown, reverse]);
 
     return (
         <div className={styles.sort}>
             {options.map((o, i) => {
-                return (
-                    <Option
-                        option={o}
-                        key={o}
-                        index={i}
-                        sortOption={sortOption}
-                        setSortOption={setSortOption}
-                    />
-                );
+                return <Option option={o} key={o} index={i} />;
             })}
-            <Arrow setEpisodesShown={setEpisodesShown} />
-        </div>
-    );
-};
-
-function Option({ option, index, setSortOption, sortOption }) {
-    return (
-        <div
-            className={styles.option}
-            onClick={() => {
-                setSortOption(option);
-            }}
-        >
-            <input
-                type="radio"
-                id={option + index}
-                name={option + " option"}
-                value={option}
-                className="sr-only"
-            />
-            <label
-                className={classnames(styles.label, {
-                    [styles.selected]: sortOption === option,
+            <Arrow
+                onClick={() => setReverse((reverse) => !reverse)}
+                className={classnames(styles.icon, {
+                    [styles.iconReverse]: reverse,
                 })}
-                htmlFor={option + index}
-            >
-                {option
-                    .split(" ")
-                    .map((word) => word[0].toUpperCase() + word.slice(1))
-                    .join(" ")}
-            </label>
+            />
         </div>
     );
-}
 
-function Arrow({ setEpisodesShown }) {
-    const [reverse, setReverse] = useState(false);
-
-    return (
-        <ArrowIcon
-            onClick={() => {
-                setEpisodesShown((episodesShown) => [...episodesShown].reverse());
-                setReverse((reverse) => !reverse);
-            }}
-            className={classnames(styles.icon, {
-                [styles.iconReverse]: reverse,
-            })}
-        />
-    );
-}
+    function Option({ option, index }) {
+        return (
+            <div
+                className={styles.option}
+                onClick={() => {
+                    setSortOption(option);
+                }}
+            >
+                <input
+                    type="radio"
+                    id={option + index}
+                    name={option + " option"}
+                    value={option}
+                    className="sr-only"
+                />
+                <label
+                    className={classnames(styles.label, {
+                        [styles.selected]: sortOption === option,
+                    })}
+                    htmlFor={option + index}
+                >
+                    {option
+                        .split(" ")
+                        .map((word) => word[0].toUpperCase() + word.slice(1))
+                        .join(" ")}
+                </label>
+            </div>
+        );
+    }
+};
 
 export default Sort;
